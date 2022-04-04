c12095e0a2071dfa97d5accb23853367478e0252
Ivashin-Max <ivashinmax@gmail.com>
2022-04-04 12:46:09 +0300

    minor

--- src/actions/fetchImitation.ts
+++ src/actions/fetchImitation.ts
@@ -4,18 +4,13 @@ import { useNavigate } from "react-router-dom";
 
 const delay = () => new Promise(resolve => setTimeout(resolve, 2000));
 
-export const imitateFetch = (data: FormData) => {
-
-
-  return async () => {
-    await delay();
-    const response = {
-      status: isValidAuth(data),
-      data: data
-    }
-    return response
+export const imitateFetch = async (data: FormData) => {
+  await delay();
+  const response = {
+    status: isValidAuth(data),
+    data: data
   }
-
+  return response
 }
 
 const isValidAuth = (data: FormData) => {
